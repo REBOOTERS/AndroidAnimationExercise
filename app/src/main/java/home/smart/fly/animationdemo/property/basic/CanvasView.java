1b2fd3804db638a2c9aee40a8f6ee31e2af9790b
REBOOTERS <foreverlove.zyl@163.com>
2016-08-12 18:24:08 +0800

    Canvas 动画

--- app/src/main/java/home/smart/fly/animationdemo/property/basic/CanvasView.java
+++ app/src/main/java/home/smart/fly/animationdemo/property/basic/CanvasView.java
@@ -11,8 +11,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
-import home.smart.fly.animationdemo.property.basic.BezierEvaluator;
-
 /**
  * Created by co-mall on 2016/8/12.
  */
