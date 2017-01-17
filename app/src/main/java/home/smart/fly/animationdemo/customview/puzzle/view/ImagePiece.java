71b55d6c86924e48829618130ae7888fd3028e40
REBOOTERS <foreverlove.zyl@163.com>
2017-01-17 18:13:24 +0800

    Puzzle Game add

---
+++ app/src/main/java/home/smart/fly/animationdemo/customview/puzzle/view/ImagePiece.java
@@ -0,0 +1,9 @@
+package home.smart.fly.animationdemo.customview.puzzle.view;
+
+import android.graphics.Bitmap;
+
+public class ImagePiece
+{
+	public int index = 0;
+	public Bitmap bitmap = null;
+}
---
+++ app/src/main/java/home/smart/fly/animationdemo/customview/puzzle/view/ImageSplitter.java
@@ -0,0 +1,47 @@
+package home.smart.fly.animationdemo.customview.puzzle.view;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageSplitter
+{
+	/**
+	 * 将图片切成 , piece *piece
+	 * 
+	 * @param bitmap
+	 * @param piece
+	 * @return
+	 */
+	public static List<ImagePiece> split(Bitmap bitmap, int piece)
+	{
+
+		List<ImagePiece> pieces = new ArrayList<ImagePiece>(piece * piece);
+
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
+
+		int pieceWidth = Math.min(width, height) / piece;
+
+		for (int i = 0; i < piece; i++)
+		{
+			for (int j = 0; j < piece; j++)
+			{
+				ImagePiece imagePiece = new ImagePiece();
+				imagePiece.index = j + i * piece;
+				
+				Log.e("TAG", "imagePiece.index" + (j + i * piece));
+				
+				int xValue = j * pieceWidth;
+				int yValue = i * pieceWidth;
+				
+				imagePiece.bitmap = Bitmap.createBitmap(bitmap, xValue, yValue,
+						pieceWidth, pieceWidth);
+				pieces.add(imagePiece);
+			}
+		}
+		return pieces;
+	}
+}
