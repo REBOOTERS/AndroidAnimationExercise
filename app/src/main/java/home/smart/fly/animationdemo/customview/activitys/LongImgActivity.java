dc0daecaa5dda519997cc124185ec5d84fcc9e4a
REBOOTERS <foreverlove.zyl@163.com>
2017-03-12 21:27:50 +0800

    Update LongImagView

--- app/src/main/java/home/smart/fly/animationdemo/customview/activitys/LongImgActivity.java
+++ app/src/main/java/home/smart/fly/animationdemo/customview/activitys/LongImgActivity.java
@@ -7,36 +7,29 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ImageView;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 import home.smart.fly.animationdemo.R;
-import home.smart.fly.animationdemo.customview.longimgview.LongImageView;
+
+
 
 public class LongImgActivity extends AppCompatActivity {
     private static final String TAG = "LongImgActivity";
     private static final float M_RATE = 1024 * 1024;
-    private ImageView img;
-    private LongImageView mImg;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_larger_img);
-//        img = (ImageView) findViewById(R.id.img);
-//        loadLargeImg();
-        mImg = (LongImageView) findViewById(R.id.img);
-//        mImg.setInputStream(getResources().openRawResource(R.raw.story));
-        InputStream inputStream = null;
-        try {
-            inputStream = getAssets().open("world.jpg");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        mImg.setInputStream(inputStream);
+        SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
+        imageView.setImage(ImageSource.resource(R.raw.story));
+
     }
 
     private void loadLargeImg() {
@@ -54,7 +47,6 @@ public class LongImgActivity extends AppCompatActivity {
             Rect rect = new Rect(0, 0, imgWidth, imgHeight / 2);
             Bitmap bitmap = regionDecoder.decodeRegion(rect, options);
             Log.e(TAG, "Momory count: " + bitmap.getByteCount() / M_RATE + "M");
-            img.setImageBitmap(bitmap);
         } catch (IOException e) {
             e.printStackTrace();
         }
