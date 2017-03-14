0f2d3f0cc1afbab8c19ca868264c6c33264f4c62
REBOOTERS <foreverlove.zyl@163.com>
2017-03-14 09:40:32 +0800

    test time

--- app/src/main/java/home/smart/fly/animationdemo/customview/activitys/LongImgActivity.java
+++ app/src/main/java/home/smart/fly/animationdemo/customview/activitys/LongImgActivity.java
@@ -2,9 +2,14 @@ package home.smart.fly.animationdemo.customview.activitys;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import home.smart.fly.animationdemo.R;
 
@@ -20,6 +25,28 @@ public class LongImgActivity extends AppCompatActivity {
         setContentView(R.layout.activity_larger_img);
         SubsamplingScaleImageView imageView = (SubsamplingScaleImageView) findViewById(R.id.imageView);
         imageView.setImage(ImageSource.resource(R.raw.story));
+        Test();
+    }
+
+    private void Test() {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String timeStr = format.format(date);
+
+        Log.e(TAG, "Test: " + timeStr);
+
+        try {
+            Date newDate = format.parse("2017-03-12 11:17:33");
+
+            double result = -(newDate.getTime() - date.getTime()) / (1000 * 24 * 3600.0f);
+
+            Log.e(TAG, "Test: " + result);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
