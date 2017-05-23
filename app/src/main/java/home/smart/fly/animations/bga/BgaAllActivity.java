6bccea1528ea6fcb876d94965d7a8d2a5bbc853d
zhuyongqing <zhuyongqing@co-mall.com>
2017-05-23 10:35:25 +0800

    some change

---
+++ app/src/main/java/home/smart/fly/animations/bga/BgaAllActivity.java
@@ -0,0 +1,20 @@
+package home.smart.fly.animations.bga;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import home.smart.fly.animations.R;
+
+
+public class BgaAllActivity extends AppCompatActivity {
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bga_all);
+    }
+
+
+}
--- app/src/main/java/home/smart/fly/animations/fragments/OtherFragment.java
+++ app/src/main/java/home/smart/fly/animations/fragments/OtherFragment.java
@@ -21,6 +21,7 @@ import home.smart.fly.animations.R;
 import home.smart.fly.animations.activity.CameraActivity;
 import home.smart.fly.animations.activity.ImgCacheActivity;
 import home.smart.fly.animations.activity.WeiXinGalleryActivity;
+import home.smart.fly.animations.bga.BgaAllActivity;
 import home.smart.fly.animations.customview.bottomsheet.BottomSheetActivity;
 import home.smart.fly.animations.activity.jianshu.FakeJianShuActivity;
 import home.smart.fly.animations.customview.puzzle.PuzzleActivity;
@@ -64,6 +65,7 @@ public class OtherFragment extends Fragment {
         demos.add(new ItemInfo(R.string.bottomSheet, ImgCacheActivity.class));
         demos.add(new ItemInfo(R.string.jianshu, FakeJianShuActivity.class));
 //        demos.add(new ItemInfo(R.string.bottomSheet, FakeFootballActivity.class));
+        demos.add(new ItemInfo(R.string.app_name, BgaAllActivity.class));
         demos.add(new ItemInfo(R.string.app_name, WeiXinGalleryActivity.class));
         demos.add(new ItemInfo(R.string.app_name, BaseRecyclerViewActivity.class));
         recyclerView = V.f(rootView, R.id.recyclerView);
