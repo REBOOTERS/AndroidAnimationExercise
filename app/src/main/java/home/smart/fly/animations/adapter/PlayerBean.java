4b15bf23e017e702b07ff106145ce9600b229492
REBOOTERS <foreverlove.zyl@163.com>
2017-07-18 22:27:03 +0800

    add players pic

---
+++ app/src/main/java/home/smart/fly/animations/adapter/PlayerBean.java
@@ -0,0 +1,42 @@
+package home.smart.fly.animations.adapter;
+
+/**
+ * Created by engineer on 2017/7/18.
+ */
+
+public class PlayerBean {
+
+    /**
+     * person_id : 119
+     * person_img : http://img.dongqiudi.com/data/personpic/119.png
+     * name : 梅西
+     */
+
+    private String person_id;
+    private String person_img;
+    private String name;
+
+    public String getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(String person_id) {
+        this.person_id = person_id;
+    }
+
+    public String getPerson_img() {
+        return person_img;
+    }
+
+    public void setPerson_img(String person_img) {
+        this.person_img = person_img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
