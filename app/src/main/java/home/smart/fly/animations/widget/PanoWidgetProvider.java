faf54202eac2710e025e5837565d069e81fa4cd1
REBOOTERS <foreverlove.zyl@163.com>
2017-08-03 17:45:14 +0800

    android widget

---
+++ app/src/main/java/home/smart/fly/animations/widget/PanoWidgetProvider.java
@@ -0,0 +1,52 @@
+package home.smart.fly.animations.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import home.smart.fly.animations.R;
+import home.smart.fly.animations.activity.CollegeActivity;
+
+/**
+ * Created by Rookie on 2017/8/3.
+ */
+
+public class PanoWidgetProvider extends AppWidgetProvider {
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+
+        // Create an Intent to launch ExampleActivity
+        Intent intent = new Intent(context, CollegeActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Get the layout for the App Widget and attach an on-click listener
+        // to the button
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget_layout);
+        views.setOnClickPendingIntent(R.id.image, pendingIntent);
+
+        // Tell the AppWidgetManager to perform an update on the current app widget
+        appWidgetManager.updateAppWidget(appWidgetIds, views);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+}
