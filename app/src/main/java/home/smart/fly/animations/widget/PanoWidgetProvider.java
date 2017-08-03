a14ad201b2d26526c13d2c1e770ebe3acc15b589
REBOOTERS <foreverlove.zyl@163.com>
2017-08-03 18:25:15 +0800

    update widget

--- app/src/main/java/home/smart/fly/animations/widget/PanoWidgetProvider.java
+++ app/src/main/java/home/smart/fly/animations/widget/PanoWidgetProvider.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import home.smart.fly.animations.R;
 import home.smart.fly.animations.activity.CollegeActivity;
@@ -16,13 +17,23 @@ import home.smart.fly.animations.activity.CollegeActivity;
 
 public class PanoWidgetProvider extends AppWidgetProvider {
 
+    private static final String ACTION_REFRESH = "home.smart.fly.animations.widget.action.REFRESH";
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction().equals(ACTION_REFRESH)) {
+            Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
 
-        // Create an Intent to launch ExampleActivity
+        // Create an Intent to launch CollegeActivity
         Intent intent = new Intent(context, CollegeActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
@@ -30,6 +41,13 @@ public class PanoWidgetProvider extends AppWidgetProvider {
         // to the button
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget_layout);
         views.setOnClickPendingIntent(R.id.image, pendingIntent);
+
+
+        Intent mIntent = new Intent();
+        mIntent.setAction(ACTION_REFRESH);
+        PendingIntent refreshPendingIntent = PendingIntent.getBroadcast(context, 0, mIntent, 0);
+        views.setOnClickPendingIntent(R.id.widget_refresh, refreshPendingIntent);
+
 
         // Tell the AppWidgetManager to perform an update on the current app widget
         appWidgetManager.updateAppWidget(appWidgetIds, views);
