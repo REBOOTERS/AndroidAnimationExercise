55206b666badbc276a58861a137564b63367d9f4
REBOOTERS <foreverlove.zyl@163.com>
2017-12-22 10:10:10 +0800

    fix bugs

--- app/src/main/java/home/smart/fly/animations/activity/QQMainAcitivity.java
+++ app/src/main/java/home/smart/fly/animations/activity/QQMainAcitivity.java
@@ -1,7 +1,9 @@
 package home.smart.fly.animations.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import home.smart.fly.animations.R;
 
 public class QQMainAcitivity extends AppCompatActivity {
 
