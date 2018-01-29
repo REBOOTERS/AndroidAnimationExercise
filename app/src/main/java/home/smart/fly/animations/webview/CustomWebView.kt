61bb995ec562800f33c6e00a831d26774ce768f0
REBOOTERS <foreverlove.zyl@163.com>
2018-01-29 21:50:19 +0800

    update info

---
+++ app/src/main/java/home/smart/fly/animations/webview/CustomWebView.kt
@@ -0,0 +1,71 @@
+package home.smart.fly.animations.webview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.webkit.WebView
+
+/**
+ * author : engineer
+ * e-mail : yingkongshi11@gmail.com
+ * time   : 2018/1/29
+ * desc   :
+ * version: 1.0
+ */
+class CustomWebView : WebView {
+
+    private val TAG: String = "CustomWebView"
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+
+    constructor(context: Context, attributeSet: AttributeSet, defaultStyle: Int) : super(context,
+            attributeSet, defaultStyle)
+
+    var oldY: Float = 0.0f
+    var t: Int = 0
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.actionMasked) {
+            MotionEvent.ACTION_MOVE -> {
+                var Y = event.y
+                var deltaY: Float = Y - oldY
+
+                Log.e(TAG, "deltaY==" + deltaY)
+
+
+
+
+                if (deltaY > 0 && this.t == 0) {
+                    parent.parent.requestDisallowInterceptTouchEvent(false)
+                }
+            }
+
+            MotionEvent.ACTION_DOWN -> {
+                parent.parent.requestDisallowInterceptTouchEvent(true)
+                oldY = event.y
+            }
+            MotionEvent.ACTION_UP -> {
+                parent.parent.requestDisallowInterceptTouchEvent(true)
+            }
+            else -> {
+
+            }
+
+        }
+        return super.onTouchEvent(event)
+    }
+
+    override fun onScrollChanged(l: Int, t: Int, oldl: Int, oldt: Int) {
+        Log.e(TAG, "t=" + t)
+        this.t = t
+        super.onScrollChanged(l, t, oldl, oldt)
+
+    }
+}
+
+
+
--- app/src/main/java/home/smart/fly/animations/webview/NestedWebViewActivity.kt
+++ app/src/main/java/home/smart/fly/animations/webview/NestedWebViewActivity.kt
@@ -4,8 +4,6 @@ import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.MotionEvent
-import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -31,38 +29,19 @@ class NestedWebViewActivity : AppCompatActivity() {
             }
         })
 
-        webView.setOnTouchListener { v, event ->
-
-        }
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             webView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
                 Log.e(TAG, "webView---------scrollY=" + scrollY)
             }
         }
 
-
-
-        scrollView.isEnabled = false
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            scrollView.setOnScrollChangeListener(object : View.OnScrollChangeListener {
-                override fun onScrollChange(v: View?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int) {
-                    Log.e(TAG, "ScrollView-scrollY=" + scrollY)
-                }
-
-            })
+            scrollView.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+                Log.e(TAG, "ScrollView-scrollY=" + scrollY)
+            }
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            scrollView.addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
-                override fun onLayoutChange(v: View?, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int) {
-                    Log.e(TAG, "ScrollView-top=" + top)
 
-                }
-
-            })
-        }
 
     }
 
