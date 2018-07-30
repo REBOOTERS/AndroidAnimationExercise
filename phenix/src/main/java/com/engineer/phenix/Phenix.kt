c790375254d93edd4fb9e06311c9505ffe5b0ba2
REBOOTERS <foreverlove.zyl@163.com>
2018-07-30 22:05:31 +0800

    update ui

--- phenix/src/main/java/com/engineer/phenix/Phenix.kt
+++ phenix/src/main/java/com/engineer/phenix/Phenix.kt
@@ -9,35 +9,35 @@ import com.engineer.phenix.bean.ImageBean
  * @date: 2018-07-22 21:07
  * @version V1.0
  */
-
+// https://github.com/aizuzi/FastAdapter
 class Phenix {
 
     private lateinit var context: Context
-    private lateinit var datas:MutableList<ImageBean>
+    private lateinit var datas: MutableList<ImageBean>
 
     private constructor()
 
     companion object {
-        fun getInstance ():Phenix {
+        fun getInstance(): Phenix {
             return Holder.INSTANCE
         }
     }
 
     private object Holder {
-        val INSTANCE  = Phenix()
+        val INSTANCE = Phenix()
     }
 
-    public fun with(context: Context): Phenix {
+    fun with(context: Context): Phenix {
         this.context = context
         return this
     }
 
-    public fun load(datas:MutableList<ImageBean>): ParamBuilder{
+    fun load(datas: MutableList<ImageBean>): ParamBuilder {
         this.datas = datas
-        return ParamBuilder(this.context,datas)
+        return ParamBuilder(this.context, datas)
     }
 
-    public fun getDatas():List<ImageBean>{
+    fun getDatas(): List<ImageBean> {
         return datas
     }
 
