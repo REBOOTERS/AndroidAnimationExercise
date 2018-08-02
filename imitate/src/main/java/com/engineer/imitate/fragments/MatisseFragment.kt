94d82c2819c995b94745a14710d133f2873ea957
REBOOTERS <yingkongshi11@gmail.com>
2018-08-02 10:20:52 +0800

    update matisse

--- imitate/src/main/java/com/engineer/imitate/fragments/MatisseFragment.kt
+++ imitate/src/main/java/com/engineer/imitate/fragments/MatisseFragment.kt
@@ -1,14 +1,20 @@
 package com.engineer.imitate.fragments
 
 
+import android.Manifest
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.alibaba.android.arouter.facade.annotation.Route
-
 import com.engineer.imitate.R
+import com.engineer.imitate.util.Glide4Engine
+import com.tbruyelle.rxpermissions2.RxPermissions
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.internal.entity.CaptureStrategy
 import kotlinx.android.synthetic.main.fragment_matisse.*
 
 
@@ -28,7 +34,21 @@ class MatisseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         button.setOnClickListener {
-
+            val permissions = RxPermissions(this)
+            permissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                    .subscribe {
+                        Matisse.from(this)
+                                .choose(MimeType.ofAll(), false)
+                                .countable(true)
+                                .capture(true)
+                                .captureStrategy(
+                                        CaptureStrategy(true, context!!.packageName + ".fileprovider"))
+                                .maxSelectable(9)
+                                .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                                .thumbnailScale(0.85f)
+                                .imageEngine(Glide4Engine())
+                                .forResult(100)
+                    }
         }
     }
 
