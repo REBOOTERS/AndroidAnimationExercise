942eb934f829fa68625af38424d1bdc9ff839686
REBOOTERS <yingkongshi11@gmail.com>
2018-12-02 20:39:54 +0800

    update scheme

--- app/src/main/java/home/smart/fly/animations/ui/activity/LaunchOtherAppActivity.java
+++ app/src/main/java/home/smart/fly/animations/ui/activity/LaunchOtherAppActivity.java
@@ -1,14 +1,26 @@
 package home.smart.fly.animations.ui.activity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.Set;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import home.smart.fly.animations.R;
+import java8.util.Optional;
+import java8.util.function.Consumer;
 
-public class LaunchOtherAppActivity extends AppCompatActivity {
+public class LaunchOtherAppActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.webView)
     WebView mWebView;
@@ -20,6 +32,13 @@ public class LaunchOtherAppActivity extends AppCompatActivity {
         setContentView(R.layout.activity_launch_other_app);
         ButterKnife.bind(this);
         mWebView.loadUrl(LOCAL_URL);
+
+        findViewById(R.id.url1).setOnClickListener(this);
+        findViewById(R.id.url2).setOnClickListener(this);
+        findViewById(R.id.url3).setOnClickListener(this);
+        findViewById(R.id.url4).setOnClickListener(this);
+        findViewById(R.id.url5).setOnClickListener(this);
+        findViewById(R.id.custom_scheme_and_host).setOnClickListener(this);
     }
 
 
@@ -29,6 +48,41 @@ public class LaunchOtherAppActivity extends AppCompatActivity {
             mWebView.goBack();
         } else {
             super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView textView = findViewById(v.getId());
+        String result = textView.getText().toString().trim();
+
+        Uri uri = Uri.parse(result);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("uri==").append(uri).append("\n");
+        sb.append("scheme==").append(uri.getScheme()).append("\n");
+        sb.append("host==").append(uri.getHost()).append("\n");
+        sb.append("path==").append(uri.getPath()).append("\n");
+        sb.append("pathSegment==").append(Arrays.toString(uri.getPathSegments().toArray())).append("\n");
+        sb.append("query==").append(uri.getQuery()).append("\n");
+
+        if (uri.isHierarchical()) {
+            Optional.ofNullable(uri.getQueryParameterNames())
+                    .ifPresent(strings -> sb.append("QueryParameterNames==").append(strings.toString()).append("\n"));
+        }
+
+
+        Toast.makeText(this, sb.toString(), Toast.LENGTH_LONG).show();
+
+        if (v.getId() == R.id.custom_scheme_and_host) {
+            Intent intent = new Intent();
+            intent.setAction("my_action");
+            intent.setData(Uri.parse(result));
+            ActivityInfo activityInfo = intent.resolveActivityInfo(getPackageManager(), PackageManager.MATCH_DEFAULT_ONLY);
+            Optional.ofNullable(activityInfo)
+                    .ifPresent( activityInfo1 -> {
+                        startActivity(intent);});
         }
 
     }
