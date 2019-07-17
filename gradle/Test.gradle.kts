be8f7e04d4ae1b3715d21e492a0a7f10f72cdf82
REBOOTERS <yingkongshi11@gmail.com>
2019-07-17 14:37:18 +0800

    add jacoco test coverage

--- gradle/Test.gradle.kts
+++ gradle/Test.gradle.kts
@@ -4,12 +4,7 @@ fun calc_module() {
     println("project ${proj.configurations}")
     proj.configurations.forEach { configuration ->
         println("configure==" + configuration.name)
-        println("configure==" + configuration.hierarchy)
-        println("")
-        var config = configuration
     }
-
-//    val configuration = proj.configurations.getByName("kotlinCompilerClasspath")
 //    println("resolver ==" + configuration.resolvedConfiguration)
 //    configuration.resolvedConfiguration.resolvedArtifacts.forEach { resolvedArtifact ->
 //        println("resolve==" + resolvedArtifact.name)
