b76dc52fc88c7975b83d8cd1d12c0ef3f10660a8
REBOOTERS <yingkongshi11@gmail.com>
2019-07-24 17:24:44 +0800

    add a transform

--- buildSrc/src/main/groovy/com/engineer/plugin/PhoenixPlugin.groovy
+++ buildSrc/src/main/groovy/com/engineer/plugin/PhoenixPlugin.groovy
@@ -1,9 +1,11 @@
 package com.engineer.plugin
 
+import com.android.build.gradle.AppExtension
 import com.engineer.plugin.actions.CalculateAction
 import com.engineer.plugin.actions.RenameAction
-import com.engineer.plugin.extensions.PhoenixExtension
 import com.engineer.plugin.actions.TaskTimeAction
+import com.engineer.plugin.extensions.PhoenixExtension
+import com.engineer.plugin.transforms.InsertLogTransform
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -13,7 +15,7 @@ class PhoenixPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
 
-        project.extensions.create('phoenix', PhoenixExtension.class,project.objects)
+        project.extensions.create('phoenix', PhoenixExtension.class, project.objects)
 
 
         project.afterEvaluate {
@@ -40,6 +42,11 @@ class PhoenixPlugin implements Plugin<Project> {
 
         TaskTimeAction.apply(project)
 
+        def appExtension = project.extensions.getByName("android")
+        if (appExtension != null) {
+            if (appExtension instanceof AppExtension) {
+                appExtension.registerTransform(new InsertLogTransform(project))
+            }
+        }
     }
-
 }
---
+++ buildSrc/src/main/groovy/com/engineer/plugin/transforms/InsertLogTransform.groovy
@@ -0,0 +1,42 @@
+package com.engineer.plugin.transforms
+
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInvocation
+import com.android.build.gradle.internal.pipeline.TransformManager
+import org.gradle.api.Project
+
+class InsertLogTransform extends Transform {
+
+    private Project project
+
+    InsertLogTransform(Project project) {
+        this.project = project
+    }
+
+    @Override
+    String getName() {
+        return "insert-log-transform"
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    @Override
+    void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        super.transform(transformInvocation)
+    }
+}
