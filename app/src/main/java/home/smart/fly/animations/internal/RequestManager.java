38d92f4fb777ced191c105da464c87193c46b1e4
REBOOTERS <yingkongshi11@gmail.com>
2019-07-29 17:49:55 +0800

    asm success

---
+++ app/src/main/java/home/smart/fly/animations/internal/RequestManager.java
@@ -0,0 +1,16 @@
+package home.smart.fly.animations.internal;
+
+import android.util.Log;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author zhuyongging @ Zhihu Inc.
+ * @since 07-29-2019
+ */
+public class RequestManager {
+
+    public String load(@Nullable String string) {
+        Log.e("zyq", "qq");
+        return "1";
+    }
+}
--- buildSrc/src/main/groovy/com/engineer/plugin/transforms/InsertLogTransform.groovy
+++ buildSrc/src/main/groovy/com/engineer/plugin/transforms/InsertLogTransform.groovy
@@ -1,15 +1,21 @@
 package com.engineer.plugin.transforms
 
-import com.android.build.api.transform.Format
-import com.android.build.api.transform.QualifiedContent
-import com.android.build.api.transform.Transform
-import com.android.build.api.transform.TransformException
-import com.android.build.api.transform.TransformInvocation
+import com.android.build.api.transform.*
 import com.android.build.gradle.internal.pipeline.TransformManager
+import com.google.common.io.ByteStreams
 import org.apache.commons.io.FileUtils
 import org.gradle.api.Project
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
 
+import java.util.jar.JarEntry
 import java.util.jar.JarFile
+import java.util.jar.JarOutputStream
+import java.util.zip.ZipEntry
 
 class InsertLogTransform extends Transform {
 
@@ -71,25 +77,129 @@ class InsertLogTransform extends Transform {
                     Format.JAR
             )
 
-            hack(jar.name, src, dest)
+            String jarName = jar.name
 
-            FileUtils.copyFile(src, dest)
+            if (jarName.contains(TARGET_MODULE_NAME)) {
+                def path = src.absolutePath
+                println("find glide " + path)
+                hack(src, dest)
+            } else {
+                FileUtils.copyFile(src, dest)
+            }
         }
     }
 
 
     private static final String TARGET_MODULE_NAME = "com.github.bumptech.glide:glide"
+    private static final String target = "com/bumptech/glide/RequestManager.class"
 
-    private static hack(String name, File sourceFile, File destFile) {
-        def path = sourceFile.absolutePath
-        if (name.contains(TARGET_MODULE_NAME)) {
-            println("name ==" + name)
-            println("find glide " + path)
+    private static hack(File sourceFile, File destFile) {
 
-            JarFile file = new JarFile(sourceFile)
+        String temp = sourceFile.absolutePath + ".temp"
+        File tempFile = new File(temp)
+        if (tempFile.exists()) {
+            tempFile.delete()
         }
 
+        JarOutputStream outputStream = new JarOutputStream(new FileOutputStream(tempFile))
+
+        JarFile jarFile = new JarFile(sourceFile)
+        Enumeration<JarEntry> entries = jarFile.entries()
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = entries.nextElement()
+            String name = jarEntry.name
+
+            InputStream inputStream = jarFile.getInputStream(jarEntry)
+            ZipEntry zipEntry = new ZipEntry(name)
+            outputStream.putNextEntry(zipEntry)
+
+            if (name == target) {
+                // todo hack
+                ClassReader classReader = new ClassReader(inputStream)
+                ClassWriter classWriter = new ClassWriter(classReader, 0)
+                ClassVisitor classVisitor = new MyVisitor(Opcodes.ASM6, classWriter)
+                classReader.accept(classVisitor, ClassReader.EXPAND_FRAMES)
+                byte[] bytes = classWriter.toByteArray()
+                if (bytes != null) {
+                    outputStream.write(bytes)
+                } else {
+                    println("sorry bytes is null")
+                    ByteStreams.copy(inputStream, outputStream)
+                }
+
+            } else {
+                ByteStreams.copy(inputStream, outputStream)
+            }
+            inputStream.close()
+            outputStream.closeEntry()
+        }
+        outputStream.close()
+        jarFile.close()
+
+        if (sourceFile.exists()) {
+            sourceFile.delete()
+        }
+        tempFile.renameTo(sourceFile)
+        FileUtils.copyFile(sourceFile, destFile)
     }
 
+    static class MyVisitor extends ClassVisitor {
+
+        MyVisitor(int api, ClassVisitor cv) {
+            super(api, cv)
+        }
+
+        void visit(int version, int access, String name, String signature,
+                   String superName, String[] interfaces) {
+            super.visit(version, access, name, signature, superName, interfaces)
+        }
+
+        @Override
+        MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+            Type type = Type.getType(desc)
+
+
+            println("acce==$access")
+            println("name==$name")
+            println("desc==$desc")
+            println("type==$type")
+            if (type != null && type.elementType != null ) {
+                println("intr=${type.elementType.hasProperty("internalName")}")
+
+            }else {
+                println("null ")
+            }
+            println("sign==$signature")
+
+
+            MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions)
+
+            // && type.elementType.internalName == "java/lang/String"
+            if (name == "load") {
+                mv = new HackMethodVisitor(Opcodes.ASM6, mv)
+            }
+            return mv
+        }
+    }
+
+    static class HackMethodVisitor extends MethodVisitor {
+
+        HackMethodVisitor(int api, MethodVisitor mv) {
+            super(api, mv)
+        }
+
+        @Override
+        void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+//            println("TestMethodVisitor, owner = " + owner + ", name = " + name);
+            //方法执行之前打印
+            mv.visitLdcInsn(" zyq")
+            mv.visitLdcInsn(" [ASM 测试] method in " + owner + " ,name=" + name)
+            mv.visitMethodInsn(Opcodes.INVOKESTATIC,
+                    "android/util/Log", "e", "(Ljava/lang/String;Ljava/lang/String;)I", false)
+            mv.visitInsn(Opcodes.POP)
+
+            super.visitMethodInsn(opcode, owner, name, desc, itf)
+        }
+    }
 
 }
