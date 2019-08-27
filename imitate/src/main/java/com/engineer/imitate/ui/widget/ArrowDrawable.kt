10b5188f02a238614fa8cc262f1bcd0113b78379
REBOOTERS <yingkongshi11@gmail.com>
2019-08-27 18:26:32 +0800

    normal update

---
+++ imitate/src/main/java/com/engineer/imitate/ui/widget/ArrowDrawable.kt
@@ -0,0 +1,20 @@
+package com.engineer.imitate.ui.widget
+
+import android.graphics.Canvas
+import android.graphics.Outline
+import android.graphics.Paint
+import android.graphics.drawable.shapes.RectShape
+
+/**
+ * @author zhuyongging @ Zhihu Inc.
+ * @since 08-27-2019
+ */
+class ArrowDrawable:RectShape() {
+    override fun draw(canvas: Canvas?, paint: Paint?) {
+        super.draw(canvas, paint)
+    }
+
+    override fun getOutline(outline: Outline) {
+        super.getOutline(outline)
+    }
+}
