3dfdd5ef19448f20f8c8d91ec356a4d4dd3fbc0a
REBOOTERS <yingkongshi11@gmail.com>
2019-11-27 15:55:45 +0800

    high level function

--- imitate/src/main/java/com/engineer/imitate/ImitateApplication.kt
+++ imitate/src/main/java/com/engineer/imitate/ImitateApplication.kt
@@ -49,5 +49,55 @@ class ImitateApplication : Application() {
         } else {
             println("result is null")
         }
+
+        testHighFun()
     }
+
+
+    // <editor-fold defaultstate="collapsed" desc="高阶函数测试">
+
+    /**
+     * 高阶函数，接受参数，返回对参数的处理结果
+     */
+    private fun sum(dodo: (x: Int, y: Int) -> Int): Int {
+        val result = dodo(1, 2)
+        println(result)
+        return result
+    }
+
+    /**
+     * 高阶函数，不接受参数，只是做一件事情，类似装饰者模式的感觉
+     */
+    private fun sum1(x: Int, y: Int, dodo: () -> Unit): Int {
+        dodo()
+        return x + y
+    }
+
+    private fun sum2(x: Int, y: Int, dodo: (Int, Int) -> Int) {
+        val result = dodo(x, y)
+        println("result from sum2 $result")
+    }
+
+    private fun testHighFun() {
+        val s = sum { x, y -> x + y }
+        println("result from sum $s")
+
+
+        val a = 1
+        val b = 2
+        val sum1 = sum1(a, b, dodo = {
+            if (a < b) {
+                println("this is decoration a < b")
+            } else {
+                println("this is decoration a >= b")
+            }
+        })
+        println("result from sum1 $sum1")
+
+        sum2(a, b, dodo = { x, y ->
+            (x * y)
+        })
+    }
+    // </editor-fold>
+
 }
