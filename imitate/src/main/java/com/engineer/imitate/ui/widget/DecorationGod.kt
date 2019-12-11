211858790dc104d4e8e8468202c2a32124e27623
REBOOTERS <yingkongshi11@gmail.com>
2019-12-11 13:52:36 +0800

    new item decoration

--- imitate/src/main/java/com/engineer/imitate/ui/widget/DecorationGod.kt
+++ imitate/src/main/java/com/engineer/imitate/ui/widget/DecorationGod.kt
@@ -1,15 +1,21 @@
 package com.engineer.imitate.ui.widget
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.engineer.imitate.util.dp2px
 
 /**
- * @author zhuyongging @ Zhihu Inc.
+ * @author rookie
  * @since 12-10-2019
  */
-class DecorationGod (val context: Context):RecyclerView.ItemDecoration() {
+class DecorationGod(val context: Context) : RecyclerView.ItemDecoration() {
+
+    private val dp8 = context.dp2px(8f).toInt()
+    private val dp4 = context.dp2px(4f).toInt()
 
     override fun getItemOffsets(
         outRect: Rect,
@@ -18,6 +24,21 @@ class DecorationGod (val context: Context):RecyclerView.ItemDecoration() {
         state: RecyclerView.State
     ) {
         super.getItemOffsets(outRect, view, parent, state)
+        val pos = parent.getChildAdapterPosition(view)
+        if (pos == 0) {
+            outRect.left = dp8
+        }
+
+        outRect.right = dp8
+        outRect.top = dp4
+        outRect.bottom = dp4
+    }
+
+    override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDraw(c, parent, state)
+        c.drawColor(Color.GREEN)
+//        c.rotate(2f)
+        c.skew(0.5f, 0f)
     }
 
 }
