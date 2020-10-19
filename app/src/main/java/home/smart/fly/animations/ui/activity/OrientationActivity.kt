6fd50caf96dca7401e2de1908fc515b59dcbe72c
rookie <yingkongshi11@gmail.com>
2020-10-19 21:56:08 +0800

    try landscape and porarait

--- app/src/main/java/home/smart/fly/animations/ui/activity/OrientationActivity.kt
+++ app/src/main/java/home/smart/fly/animations/ui/activity/OrientationActivity.kt
@@ -1,13 +1,28 @@
 package home.smart.fly.animations.ui.activity
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import home.smart.fly.animations.R
+import kotlinx.android.synthetic.main.activity_orientation.*
 
 class OrientationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orientation)
+        switcher.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                switcher.text = "切竖屏"
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            } else {
+                switcher.text = "切横屏"
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        }
+        text.movementMethod = ScrollingMovementMethod.getInstance()
     }
 }
