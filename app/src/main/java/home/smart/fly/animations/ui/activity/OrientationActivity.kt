9db2ce664bdbccb77376601eeef6292041658e24
REBOOTERS <yingkongshi11@gmail.com>
2020-11-07 17:42:18 +0800

    some fix

--- app/src/main/java/home/smart/fly/animations/ui/activity/OrientationActivity.kt
+++ app/src/main/java/home/smart/fly/animations/ui/activity/OrientationActivity.kt
@@ -1,16 +1,22 @@
 package home.smart.fly.animations.ui.activity
 
+import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import android.view.Window
-import android.view.WindowManager
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import home.smart.fly.animations.R
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_orientation.*
+import java.util.concurrent.TimeUnit
 
 class OrientationActivity : AppCompatActivity() {
+    private var disposable: Disposable? = null
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_orientation)
@@ -24,5 +30,22 @@ class OrientationActivity : AppCompatActivity() {
             }
         }
         text.movementMethod = ScrollingMovementMethod.getInstance()
+
+        disposable = Observable.just(0).delay(1, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                val xy = IntArray(2)
+                pos_tv.getLocationOnScreen(xy)
+                pos_tv.text = "position info: x=${xy[0]},y=${xy[1]}"
+
+                val hour = TimeUnit.DAYS.toHours(1)
+
+                Log.e("zyq", "hour is == $hour")
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable?.dispose()
     }
 }
