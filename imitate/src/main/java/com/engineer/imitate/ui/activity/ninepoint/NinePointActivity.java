f707e3f98b11d525b186306654fc89db15f09db0
rookie <yingkongshi11@gmail.com>
2020-12-29 21:12:12 +0800

    fix all

--- imitate/src/main/java/com/engineer/imitate/ui/activity/ninepoint/NinePointActivity.java
+++ imitate/src/main/java/com/engineer/imitate/ui/activity/ninepoint/NinePointActivity.java
@@ -45,7 +45,7 @@ public class NinePointActivity extends AppCompatActivity implements View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //requestWindowFeature(Window.FEATURE_NO_TITLE); //隐藏标题栏(不起作用)
-        getSupportActionBar().hide();  //隐藏标题栏
+//        getSupportActionBar().hide();  //隐藏标题栏
         setContentView(R.layout.activity_nine_point);
 
         initView();
