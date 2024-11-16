913f9440a075ecff7154be598029fe175b85e255
rookie <yingkongshi11@gmail.com>
2024-11-16 22:15:11 +0800

    add a delegate example

---
+++ subs/compose/src/main/java/com/engineer/compose/uitls/DelegateExample.kt
@@ -0,0 +1,19 @@
+package com.engineer.compose.uitls
+
+import kotlin.reflect.KProperty
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    }
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("$value has been assigned to '${property.name}' in $thisRef.")
+    }
+}
+
+fun main() {
+    var p by Delegate()
+    println(p)
+    p = "hello"
+    println(p)
+}
